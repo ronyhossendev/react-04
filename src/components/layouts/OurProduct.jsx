12d008f375e9c3b58b5800a8fbb96030b54aad0a
123rony123 <mdronyhossen201@gmail.com>
2026-03-09 22:16:25 +0600

    up

--- src/components/layouts/OurProduct.jsx
+++ src/components/layouts/OurProduct.jsx
@@ -7,24 +7,15 @@ const OurProduct = () => {
         <Container>
             <div>
                 <h1 className="text-[#363024] font-openSans font-extrabold text-[48px] pt-[150px]">Our Product</h1>
-               <div className="flex justify-end gap-4 ">
-                <div className="bg-[#524D43] w-[56px] h-[56px]"></div>
-                <div className="bg-[#D6A033] w-[56px] h-[56px]"></div>
-                </div>
+               
             </div>
        <div className='flex justify-between pt-[64px]'>
         
-        <div className='bg-[#C4C4C4] w-[270px] h-[329px]'>
-          
+        <div className='bg-[#C4C4C4] w-[270px] h-[329px]'></div>
+        <div className='bg-[#C4C4C4] w-[270px] h-[329px]'></div>
+        <div className='bg-[#C4C4C4] w-[270px] h-[329px]'></div>
+        <div className='bg-[#C4C4C4] w-[270px] h-[329px]'></div>
         </div>
-                 
-             
-        <div className='bg-[#C4C4C4] w-[270px] h-[329px]'></div>
-        <div className='bg-[#C4C4C4] w-[270px] h-[329px]'></div>
-        <div className='bg-[#C4C4C4] w-[270px] h-[329px]'></div>
-        
-
-       </div>
 
         </Container>
     </section>
