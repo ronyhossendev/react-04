546983f9be1bd6609da1dec1ef7dd0cdebbcb29d
123rony123 <mdronyhossen201@gmail.com>
2026-03-09 22:19:23 +0600

    up

--- src/components/layouts/OurProduct.jsx
+++ src/components/layouts/OurProduct.jsx
@@ -5,17 +5,7 @@ const OurProduct = () => {
   return (
     <section className="bg-[#FCFBF9] w-[1920px] h-[835px]">
         <Container>
-            <div>
-                <h1 className="text-[#363024] font-openSans font-extrabold text-[48px] pt-[150px]">Our Product</h1>
-               
-            </div>
-       <div className='flex justify-between pt-[64px]'>
-        
-        <div className='bg-[#C4C4C4] w-[270px] h-[329px]'></div>
-        <div className='bg-[#C4C4C4] w-[270px] h-[329px]'></div>
-        <div className='bg-[#C4C4C4] w-[270px] h-[329px]'></div>
-        <div className='bg-[#C4C4C4] w-[270px] h-[329px]'></div>
-        </div>
+    
 
         </Container>
     </section>
